907e4da7cfa86fdf5ac6d9996efef901136a86db
bosskitti7983 <bosskitti7983@gmail.com>
2018-03-12 22:20:59 +0700

    first commit

---
+++ generate_picture.py
@@ -0,0 +1,13 @@
+#"D:\virtualenv\ROBOTICS_studios\Scripts\activate"
+
+import os
+import sys
+
+from PIL import Image
+from math import sqrt
+
+'''*************************************************
+*                                                  *
+*                 config tensorflow                *
+*                                                  *
+*************************************************'''
