0efeed7f4733f48e444594caa600cab04bcedf1c
bosskitti7983 <bosskitti7983@gmail.com>
2018-03-13 04:56:17 +0700

    begin write haar cascade logic

---
+++ run_open_cvcreatesamples.sh
@@ -0,0 +1,2 @@
+opencv_createsamples -img main_img/$1* -bg bg_pos.txt -info info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num $2
+# opencv_createsamples -info info/info.lst -num $2 -w 24 -h 24 -vec positives.vec 
