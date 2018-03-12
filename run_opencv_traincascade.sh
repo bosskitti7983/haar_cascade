0a8368bc5d6e30921cd6db8fc951944c65143709
bosskitti7983 <bosskitti7983@gmail.com>
2018-03-13 05:03:29 +0700

    lastest version

--- run_opencv_traincascade.sh
+++ run_opencv_traincascade.sh
@@ -1,1 +1,1 @@
-opencv_traincascade -data output_data -vec positives.vec -bg bg_neg.txt -npos $1 -nneg $2 -nstate $3 -mem 4096 -w 24 -h 24 
+opencv_traincascade -data output_data -vec positives.vec -bg bg_neg.txt -npos $1 -nneg $2 -nstate $3 -w 24 -h 24 
