1c0ad8d3364d036666b17ccce48861585f333d5a
bosskitti7983 <bosskitti7983@gmail.com>
2018-03-14 06:52:39 +0700

    traincascade per 1 positive

---
+++ run_opencv_createsamples.sh
@@ -0,0 +1,10 @@
+weight=24
+height=24
+# opencv_createsamples -img main_img/$1* -bg bg_pos.txt -info info/info.lst -pngoutput info -maxxangle 0.1 -maxyangle 0.1 -maxzangle 0.1 -num $2
+echo "generate positive info.lst done"
+# opencv_createsamples -info info/info.lst -num $2 -w $weight -h $height -vec positives.vec 
+opencv_createsamples -img main_img/$1* -bg bg_pos.txt -vec positives.vec -maxxangle 0.1 -maxyangle 0.1 -maxzangle 0.1 -num $2
+echo "generate vector done"
+
+# $1 selected main image
+# $2 positive number 
--- run_opencv_traincascade.sh
+++ run_opencv_traincascade.sh
@@ -1,4 +1,7 @@
-opencv_traincascade -data output_data -vec positives.vec -bg bg_neg.txt -npos $1 -nneg $2 -nstate $3 -w 24 -h 24
+weight=24
+height=24
+
+opencv_traincascade -data output_data -vec positives.vec -bg bg_neg.txt -npos $1 -nneg $2 -nstate $3 -w $weight -h $height -precalcValBufSize 2048 -precalcIdxBufSize 2048
 
 
 # $1 number of positive image
